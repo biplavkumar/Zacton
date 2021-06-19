f87fb6c816072dba31931c3e99661f56d5ae310d
Biplav Kumar <biplavkumar5@gmail.com>
2021-06-19 10:47:48 +0530

    Boot Project getting pushed

---
+++ ZactonTechnology/zacton-technology/src/test/java/com/zacton/technology/ZactonTechnologyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zacton.technology;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZactonTechnologyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
